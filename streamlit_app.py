9312653e366d16cb5f95702c3694f74da202b029
rktst110 <84590431+rktst110@users.noreply.github.com>
2023-04-21 23:30:48 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from test import main
+from google.cloud import firestore
 
 st.write('Hello world!')
 st.write('this is main page')
@@ -7,3 +8,18 @@ st.write('this is a change')
 
 
 main()
+
+
+
+# Authenticate to Firestore with the JSON account key.
+db = firestore.Client.from_service_account_json("firestore-key.json")
+
+# Create a reference to the Google post.
+doc_ref = db.collection("posts").document("Google")
+
+# Then get the data at that reference.
+doc = doc_ref.get()
+
+# Let's see what we got!
+st.write("The id is: ", doc.id)
+st.write("The contents are: ", doc.to_dict())
