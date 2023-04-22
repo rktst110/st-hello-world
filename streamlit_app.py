58033edc64dcbcba947623ec537ea089ef40a1c0
rktst110 <84590431+rktst110@users.noreply.github.com>
2023-04-22 13:13:01 +0530

    f

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,8 +30,10 @@ st.write("testing through dev neqw")
 #doc_ref = db.collection("April 2023").document("13 April 2023")
 #docs = db.collection("Test").document("DocTest")
 
-docs = db.collection(u'Test').stream()
+fdfdf
+
+docs = db.collection('Test').stream()
 
 for doc in docs:
-    print(f'{doc.id} => {doc.to_dict()}')snippets.py
+    print(f'{doc.id} => {doc.to_dict()}')
 
