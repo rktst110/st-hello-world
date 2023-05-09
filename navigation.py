e1b4c9a6b8abdbc4b6393562a6692596f85d55e3
rktst110 <84590431+rktst110@users.noreply.github.com>
2023-05-09 14:48:40 +0530

    g

--- navigation.py
+++ navigation.py
@@ -1,14 +1,21 @@
 import streamlit as st
 
-# Using object notation
-add_selectbox = st.sidebar.selectbox(
-    "How would you like to be contacted?",
-    ("Email", "Home phone", "Mobile phone")
-)
 
-# Using "with" notation
-with st.sidebar:
-    add_radio = st.radio(
-        "Choose a shipping method",
-        ("Standard (5-15 days)", "Express (2-5 days)")
+def main():
+    st.title("My Streamlit App")
+    
+    # Using object notation
+    add_selectbox = st.sidebar.selectbox(
+        "How would you like to be contacted?",
+        ("Email", "Home phone", "Mobile phone")
     )
+
+    # Using "with" notation
+    with st.sidebar:
+        add_radio = st.radio(
+            "Choose a shipping method",
+            ("Standard (5-15 days)", "Express (2-5 days)")
+        )
+        
+if __name__ == "__main__":
+    main()
